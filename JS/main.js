fb5fc4335a2a976d3bd7cb1b82f4b7f1b6b12d36
shar00yg <yu1453010977@foxmail.com>
2019-10-09 21:36:05 +0800

    main html/css/js   slideShow

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,82 @@
 // .slideshow
-var temp = 1; //用来保存哪个图片显示
-function showImg(changNum) { //让背景图片显示
-    if (Number(changNum)) {//判断点击了要显示图片的按钮
-        clearTimeout(t);
-        if(changNum==1){
-            temp+=1;
+var contain = document.querySelector('.contain');//获取需要改变left的元素
+var slide = document.querySelector('.slide');//获取需要改变left的元素
+var leftBtn = document.querySelector('.left');//获取左边按钮
+var rightBtn = document.querySelector('.right');//获取右边按钮
+
+var slideLength = slide.children.length;//轮播图个数
+var timer = null;//定时器初始为null
+var index = 0;//计数
+window.onload = function () {
+    var everyTime = 2000;//轮播的间隔时间
+    autoPlay(everyTime);//页面打开后自动开启播放
+    //当有鼠标划过时停止定时器
+    contain.onmouseenter = function () {
+        clearInterval(timer);
+    };
+    //鼠标摞开时启动定时器
+    contain.onmouseleave = function () {
+        autoPlay(everyTime);
+    };
+    //点击左边按钮
+    leftBtn.onclick = function () {
+        if (index == 0) {
+            play(-(1299 * (slideLength - 2)),slideLength - 2,0);
+            index = slideLength - 2;
+        } else {
+            index--;
+            play(-(1299 * index),index,index +1);
         }
-        else if(changNum==-1){
-            temp-=1;
+    };
+    //点击右边按钮
+    rightBtn.onclick = function () {
+        index++;
+        if (index == slideLength - 1) {
+            criticality();
+        } else {
+            if(index > slideLength-1){
+                return;
+            }
+            play(-(1299 * index),index,index -1);
         }
-        
-    }
-    document.getElementById("slideShow").style.backgroundImage = "url(./Images/slideShow-0" + temp + ".png)";
-    temp++;
-    if(temp == 5) {
-        temp = 1;
-    }
-    t = setTimeout("showImg()", 3000);//刷新时间
+    };
+
+
+};
+//自动播放
+function autoPlay(period) {
+    timer = setInterval(function () {
+        index++;
+        if (index == slideLength - 1) {
+            clearInterval(timer);
+            criticality();
+            return autoPlay(period);
+        } else {
+            if(index > slideLength-1){
+                return;
+            }
+            play(-(1299 * index),index,index -1);
+        }
+    }, period)
 }
-
-
+//轮播公共方法
+function play(left,activeIndex,emptyIndex) {
+    //1、图片移动left px；
+    slide.style.left = left + 'px';
+    slide.style.transition = 'left 1.5s';
+}
+function criticality() {
+    play(-(1299 * index),0,slideLength - 2);
+    var now=(new Date()).getTime()+1500;
+    if((new Date()).getTime()<=now){
+        rightBtn.disabled=true;
+        setTimeout(function () {
+            rightBtn.disabled=false;
+        },now-(new Date()).getTime())
+    }
+    setTimeout(function () {
+        slide.style.left = 0 + 'px';
+        slide.style.transition = 'left 0s';
+        index = 0;
+    }, 1500);
+}
